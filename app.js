0a94e0aabe02e2e564637bf8009af06b2e5c9826
eightyfour <eightyfour@thinkuseful.de>
2015-11-08 21:12:31 +0100

    add singleFolder support

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
  * Created by eightyfour on 07.10.15.
  */
 var config = require('./package').config.upload,
-    downloader = require('./lib/downloader.js');
+    downloader = require('./lib/downloader.js'),
+    clc = require('cli-color');
 
 console.log('app:config', config);
 
 if (config) {
-
     downloader.load(config, __dirname);
     downloader.onError(function (ulr) {
-        console.log('app:onError', ulr);
+        console.log(clc.red.bold('Failure ') + clc.yellow('Download failed for file'), clc.yellowBright(ulr));
     })
 }
